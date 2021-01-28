90d1d8dd6a51c6c252d5257da02849a07623bb84
yoshida riki <ab.jpnet.01@gmail.com>
2021-01-28 17:56:24 +0900

    :white_check_mark: [test]ButtonPost.spec.js
    再度test

--- test/ButtonPost.spec.js
+++ test/ButtonPost.spec.js
@@ -1,20 +1,20 @@
 import { mount } from '@vue/test-utils'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import Button from '@/components/Button.vue'
+import ButtonPost from '@/components/ButtonPost.vue'
 
 Vue.use(Vuetify)
 
 describe('Component', () => {
 	// テストの比較
 	it('is a Vue instance', () => {
-		const wrapper = mount(Button)
+		const wrapper = mount(ButtonPost)
 		expect(wrapper.isVueInstance()).toBeTruthy()
 	})
 	
 	it('Click on button calls onClick', async () => {
 		const myMock = jest.fn();
-		const wrapper = mount(Button, {
+		const wrapper = mount(ButtonPost, {
 			propsData: {
 				onClick: myMock
 			}
