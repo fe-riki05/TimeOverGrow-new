4faccc96b0eb09197dd6a301f0d0617d898a377f
yoshida riki <ab.jpnet.01@gmail.com>
2021-02-10 00:32:14 +0900

    :construction_worker: [update]CI&CD
    CI&CDファイルのESLintエラー対応

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
-import firebaseAuth from './firebase.auth'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAEFHkdyYqCLfQZC_nXAnoNRFbR_kWsvVU",
-	authDomain: "timeovergrow-d76f5.firebaseapp.com",
-	projectId: "timeovergrow-d76f5",
-	storageBucket: "timeovergrow-d76f5.appspot.com",
-	messagingSenderId: "868668025407",
-	appId: "1:868668025407:web:3790913986172449dce68a",
-	measurementId: "G-9W2YX5YWTX"
+	apiKey: 'AIzaSyAEFHkdyYqCLfQZC_nXAnoNRFbR_kWsvVU',
+	authDomain: 'timeovergrow-d76f5.firebaseapp.com',
+	projectId: 'timeovergrow-d76f5',
+	storageBucket: 'timeovergrow-d76f5.appspot.com',
+	messagingSenderId: '868668025407',
+	appId: '1:868668025407:web:3790913986172449dce68a',
+	measurementId: 'G-9W2YX5YWTX'
 }
 
 if (!firebase.apps.length) {
